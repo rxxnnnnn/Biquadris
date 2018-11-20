938b190758a9b46a8366b0eb3a2bcea7a383a48a
Apple <Apple@appledemacbook-air-5.local>
2018-11-20 13:36:59 -0500

    Initial Commit

---
+++ Biquadris/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Biquadris
+//
+//  Created by Apple on 2018-11-20.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
